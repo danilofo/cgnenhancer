8d406a99f8783911610642eef586d3672bc4c8a1
daf0rest <danilo.forastiere@outlook.com>
2017-04-23 15:15:44 +0200

    Add files via upload

---
+++ cngenhancer.py
@@ -0,0 +1,59 @@
+import sys
+
+class Context: 
+	#A class for the Context data structure;
+	#A Context is a container for related tags
+	global GlobTagList
+	def __init__(self):
+		self.tags=list()
+		#tags in the current context should be read from a file
+	def addTag(self,myTagName):
+		if myTagName in GlobTagList:
+			self.tags.append(myTagName)
+	def listTags(self):
+		return self.tags;
+		
+class Tag:
+	global UrlDatabase
+	#Tags refer to external resources (site,articles, etc...) using a unique id number
+	def __init__(self):
+		self.urls=list()
+	def addUrl(self,myUrlName):
+		if myUrlName in UrlDatabase.keys() : 
+			self.append(UrlDatabase[myUrlName])
+	def listUrls(self):
+		return self.urls;
+	
+	
+def main():
+	#Initialize global data structures 
+	GlobTagList=list()
+	ContextDict=dict()
+	#TODO: parse the file with context and tags
+	
+	
+	UrlDatabase=dict()
+	#TODO: parse the file with urls and update tags  
+	
+	#This is the main loop 
+	exitFlag=False;
+	while(not exitFlag):
+		print("Cognitive enhancer debug version")
+		validChoice=False
+		ContextChoice=input("Choose a context number or exit (X): ")
+		#TODO: parse ContextChoice variable
+		
+		if(validChoice):
+			if(ContextChoice=="X"): 
+				exitFlag=True
+		else:
+			CurrentContextName=""
+			print("Context: "+CurrentContextName)
+			#TODO: print the list of available tags
+			TagChoice=input("Choose a tag name from the list: ")
+			#TODO: parse TagChoice variable
+			#TODO: list the urls
+			
+	return
+
+if __name__=="__main__": main()
