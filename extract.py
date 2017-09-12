660c45cca9d19aa489c678e27ec9dbbc2f8e8e1a
HX4G0N <carlo.dedonno@live.com>
2017-09-12 21:14:01 +0200

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,9 +1,10 @@
 import sys
-
+from cgnenhancer import Context, Tag
 def extract(namefile):
 	UrlDatabase = dict()
 	url_list = open(namefile, 'r')
-	
+	global GlobTagList
+	GlobTagList = list()
 	#builds a dictionary with urls as keys and urls as values
 	for line in url_list:
 		junk_line, line = line.split("=", 1)
@@ -18,6 +19,21 @@ def extract(namefile):
 	for key in UrlDatabase:	#DEBUG
 		print key, UrlDatabase[key]
 		
+	TagDatabase = dict()
+	
+	for key in UrlDatabase:
+		for tag in UrlDatabase[key]:
+			TagDatabase[tag] = list()
+	for key in UrlDatabase:
+		for tag in UrlDatabase[key]:
+			TagDatabase[tag].append(key)
+
+	#creates list of Tag objects
+	GlobTagList = [Tag(TagDatabase.keys()[i]) for i in range(len(TagDatabase))]
+	for item in GlobTagList:
+		print item.name #DEBUG
+	for key in TagDatabase
+		
 	url_list.close()
 	
-extract('urls.html') #DEBUG
+extract('urls.html')
